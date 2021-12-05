096ba836ff966264144ec74c4158f9cb3bf6e805
aoaoaoaoao___________ <z.1a2oyk@gmail.com>
2021-12-05 10:47:45 +0900

    コメント欄余白調整・ほか

--- comments.php
+++ comments.php
@@ -25,13 +25,13 @@
     $aria_req = ( $req ? " aria-required='true'" : '' );
     $args = array(
         'fields' => array(
-        'author' => '<p class="c-comment__form comment-form-author">' . '<label for="author">' . __( 'Name' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label><br>' .
+        'author' => '<p>' . '<label for="author">' . __( 'Name' ) . ( $req ? ' <span class="required">*</span>' : '' ) . '</label><br>' .
         '<input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' /></p>',
-        'email' => '<p class="c-comment__form comment-form-email"><label for="email">' . __( 'Email' ) . '</label> ' . ( $req ? '<span class="required">*</span><br>' : '<br>' ) .
+        'email' => '<p><label for="email">' . __( 'Email' ) . '</label> ' . ( $req ? '<span class="required">*</span><br>' : '<br>' ) .
         '<input id="email" name="email" type="text" value="' . esc_attr( $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' /></p>',
         'url'    => '',
         ),
-        'comment_field' => '<p class="c-comment__form comment-form-comment"><label for="comment">' . _x( 'Comment', 'noun' ) . 
+        'comment_field' => '<p><label for="comment">' . _x( 'Comment', 'noun' ) . 
         '</label><br><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
         'title_reply' => '',
         'comment_notes_before' => '<p class="c-comment__form">メールアドレスは公開されませんのでご安心ください。<br />また、<span class="required">*</span> が付いている欄は必須項目となりますので、必ずご記入をお願いします。</p>',
